0a950be0ef3f81f4a4a6c0f72e0ecf1c5d424577
NurdinBakyt <bakytbekovnurdin7@gmail.com>
2025-02-16 16:00:58 +0600

    Запустили docker

--- src/main/java/org/nurdin/school/configuration/user/WebSecurityConfig.java
+++ src/main/java/org/nurdin/school/configuration/user/WebSecurityConfig.java
@@ -38,6 +38,7 @@ public class WebSecurityConfig {
                     auth.requestMatchers("/api/v1/user/**").permitAll();
                     auth.requestMatchers("/api/v1/auth/**").permitAll();
                     auth.requestMatchers("/api/v1/news/**").permitAll();
+                    auth.requestMatchers("/api/v1/user/get-all-users").permitAll();
                     auth.requestMatchers("/api/v1/news/").permitAll();
                     auth.requestMatchers("/api/v1/bidForWork/getAllBids").hasAnyAuthority("HEAD_TEACHER", "SECRETARY");
                     auth.requestMatchers("/api/v1/bidForWork/acceptBid/", "/api/v1/bidForWork/rejectTheBid").
